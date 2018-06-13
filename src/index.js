d410e14c259f24938422eb068204f25ce53e2242
Konstantin Petryaev <numminorihsf@gmail.com>
2018-06-13 09:48:25 +0700

    Add reexporting FromInternalComponentProps type

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { FormField, type FormFieldProps } from './FormField';
 import { FormFieldError, type FormFieldErrorProps, } from './FormFieldError';
 import { FormError, type FormErrorProps } from './FormError';
 import { createFieldRules } from './createFieldRules';
+import { type FromInternalComponentProps } from './createFormInternalComponent';
 
 export { Form, FormField, FormFieldError, FormError, createFieldRules };
-export type { FormProps, FormFieldProps, FormFieldErrorProps, FormErrorProps };
+export type { FormProps, FormFieldProps, FormFieldErrorProps, FormErrorProps, FromInternalComponentProps };
