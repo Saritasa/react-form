b8ba24f470b0fc10d9087d0e4a4cb5958e0f13b7
Konstantin Petryaev <numminorihsf@gmail.com>
2018-08-31 10:52:22 +0700

    Resolve issue with FormFieldError displaying

--- src/FormFieldError.js
+++ src/FormFieldError.js
@@ -23,7 +23,7 @@ export const FormFieldError = ({
 }: FormFieldErrorProps) => (
   <Field
     name={name}
-    render={({ errors, touched }) => {
+    render={({ form: { errors, touched }}) => {
       if (!touched) return null;
       if (!errors) return null;
       if (!touched[name]) return null;
